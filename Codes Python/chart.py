3c8053813b6bba4e7c085fb615d77a0075390151
Harshit <harshitkholiya1003@gmail.com>
2025-11-06 10:50:08 +0530

    Update chart.py

--- Codes Python/chart.py
+++ Codes Python/chart.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-# Charts connected to main.py 
+import matplotlib.pyplot as plt 
+
 
 class Chart:
     def __init__(self):
@@ -9,14 +9,33 @@ class Chart:
         plt.figure(figsize=(10,8))
         plt.bar(df["month"], df["Income"], label="Income")
         plt.bar(df["month"], df["Expense"], bottom=df["Income"], label="Expense")
-        plt.xlabel("Month")
+        plt.xlabel("-----Months-----")
         plt.ylabel("Amount")
         plt.title("🈷️(Monthly) 💰Income vs ❌Expense")
         plt.legend() # explain the colors 
         plt.show()
         
     
+    def acpie_chart(self,df):# df = Accounts Here
+        count=df["account_type"].value_counts() # counts each unique value  appeared
+        plt.figure(figsize=(7,7))
+        plt.pie(count.values,
+        labels=count.index)
+        plt.title("Account Types")
+        plt.legend()
+        plt.show()
+
     
+    def monthwise_expense(self,df):
+        months = df["month"]
+        expenses = df["Amount"]
+        plt.figure(figsize=(5,5))
+        plt.plot(months, expenses, marker="o")
+        plt.xlabel("Months")
+        plt.ylabel("Total Expense")
+        plt.title("Month wise  Expense")
+        plt.grid()
+        plt.show()
         
 
 
