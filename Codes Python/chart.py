d56c9eb89e9a38f5b10a827601683c5d5bdb18df
Harshit <harshitkholiya1003@gmail.com>
2025-11-06 09:22:04 +0530

    Update chart.py

--- Codes Python/chart.py
+++ Codes Python/chart.py
@@ -1,1 +1,24 @@
+import matplotlib.pyplot as plt
+# Charts connected to main.py 
+
+class Chart:
+    def __init__(self):
+        pass
+    
+    def monthly_income_chart(self, df): # df = Transactions here 
+        plt.figure(figsize=(10,8))
+        plt.bar(df["month"], df["Income"], label="Income")
+        plt.bar(df["month"], df["Expense"], bottom=df["Income"], label="Expense")
+        plt.xlabel("Month")
+        plt.ylabel("Amount")
+        plt.title("🈷️(Monthly) 💰Income vs ❌Expense")
+        plt.legend() # explain the colors 
+        plt.show()
+        
+    
+    
+        
+
+
+
 
