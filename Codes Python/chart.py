1870ed3ad343d4ad5c70b070570e06a0bcf737c7
Harshit <harshitkholiya1003@gmail.com>
2025-11-07 08:13:40 +0530

    Refactor chart methods and add top5 method

--- Codes Python/chart.py
+++ Codes Python/chart.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt 
-
+import pandas as pd
 
 class Chart:
     def __init__(self):
         pass
     
-    def monthly_income_chart(self, df): # df = Transactions here 
+    def monthly_income_chart(self, df): 
         plt.figure(figsize=(10,8))
         plt.bar(df["month"], df["Income"], label="Income")
         plt.bar(df["month"], df["Expense"], bottom=df["Income"], label="Expense")
@@ -16,7 +16,7 @@ class Chart:
         plt.show()
         
     
-    def acpie_chart(self,df):# df = Accounts Here
+    def acpie_chart(self,df):
         count=df["account_type"].value_counts() # counts each unique value  appeared
         plt.figure(figsize=(7,7))
         plt.pie(count.values,
@@ -27,6 +27,9 @@ class Chart:
 
     
     def monthwise_expense(self,df):
+        order = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+        df["month"] = pd.Categorical(df["month"], categories=order)
+        df = df.sort_values("month")
         months = df["month"]
         expenses = df["Amount"]
         plt.figure(figsize=(5,5))
@@ -36,8 +39,14 @@ class Chart:
         plt.title("Month wise  Expense")
         plt.grid()
         plt.show()
-        
 
-
+    def top5(self,df):
+        names = df["category_name"]
+        values=df["Amount"]
+        plt.figure(figsize=(5,5))
+        plt.pie(values,labels=names)
+        plt.legend()
+        plt.title("Top 5 Expense Categories")
+        plt.show()
 
 
